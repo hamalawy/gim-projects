171ee4aec0b5fb54055f9051196ab030c7f3984b
gmauer <gmauer@45c0859a-793f-11de-a93f-2d52da730ff4>
2009-07-26 07:41:23 +0000

    Property level injection seems to be working.  Could stand to flesh out the tests a little more and refactor. Also need to figure out how this works with xml config.

--- trunk/app/GIM.CastleContrib/OptionalPropertyInjectionFacility.cs
+++ trunk/app/GIM.CastleContrib/OptionalPropertyInjectionFacility.cs
@@ -8,11 +8,29 @@ namespace GIM.CastleContrib {
 
     public class OptionalPropertyInjectionFacility : AbstractFacility, IFacility {
         private bool _wirePropertiesInContainerByDefault;
+
         public OptionalPropertyInjectionFacility(bool provideByDefault) {
             _wirePropertiesInContainerByDefault = provideByDefault;
         }
+
         protected override void Init() {
             Kernel.ComponentRegistered += new ComponentDataDelegate(OnComponentRegistered);
+        }
+
+        void OnComponentRegistered(string key, IHandler handler) {
+            var model = handler.ComponentModel;
+            var propertiesToRemove = model.Properties.Where(p => ShouldRemove(p.Property, model)).ToArray();
+            propertiesToRemove.ForEach(ps =>
+                model.Properties.Remove(ps));
+        }
+
+        private bool ShouldRemove(System.Reflection.PropertyInfo pi, ComponentModel model) {
+            bool? wirePropertiesOfComponentByDefault = ToBool(model.Configuration.Attributes["wire-all-properties"]);
+            bool wireThisSpecificProperty = (model.Configuration.Attributes["wire-selected-properties"] ?? "").Split(',')
+                .Select(x => x.Trim()).Contains(pi.Name);
+            if (wireThisSpecificProperty)
+                return false;
+            return !(wirePropertiesOfComponentByDefault ?? _wirePropertiesInContainerByDefault);
         }
 
         private static bool? ToBool(string stringBool) {
@@ -20,16 +38,6 @@ namespace GIM.CastleContrib {
             bool success = Boolean.TryParse(stringBool, out res);
             return success == false ? null : res as bool?;
         }
-        private bool ShouldRemove(System.Reflection.PropertyInfo pi, ComponentModel model) {
-            bool? wirePropertiesOfComponentByDefault = ToBool(model.Configuration.Attributes["wire-all-properties"]);
 
-            return !(wirePropertiesOfComponentByDefault ?? _wirePropertiesInContainerByDefault);
-        }
-        void OnComponentRegistered(string key, IHandler handler) {
-            var model = handler.ComponentModel;
-            var propertiesToRemove = model.Properties.Where(p => ShouldRemove(p.Property, model)).ToArray();
-            propertiesToRemove.ForEach(ps=> 
-                model.Properties.Remove(ps));
-        }
     }
 }
