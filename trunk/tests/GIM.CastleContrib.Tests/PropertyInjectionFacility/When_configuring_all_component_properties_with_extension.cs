5de6a4ad32411e60a5ed31d1eb21fc94c6584756
gmauer <gmauer@45c0859a-793f-11de-a93f-2d52da730ff4>
2009-07-26 21:38:13 +0000

    Cleaned up tests so they all fit new format.  Changed pretty much everything about how the internals work

--- trunk/tests/GIM.CastleContrib.Tests/PropertyInjectionFacility/When_configuring_all_component_properties_with_extension.cs
+++ trunk/tests/GIM.CastleContrib.Tests/PropertyInjectionFacility/When_configuring_all_component_properties_with_extension.cs
@@ -1,12 +1,10 @@
 using System;
-using Castle.Windsor;
 using Castle.MicroKernel.Registration;
 using Xunit;
 
 namespace GIM.CastleContrib.Tests.PropertyInjectionFacility {
-    public class When_configuring_all_component_properties_with_extension_method : WhenTestingConfigurationOptions {
-        [Fact]
-        public void will_override_the_facility_default_and_inject() {
+    public class When_configuring_all_component_properties_with_extension : WhenTestingConfigurationOptions {
+        [Fact] public void will_override_the_facility_default_and_inject() {
             create_facility = () => new OptionalPropertyInjectionFacility(false);
             additonal_registration = reg => reg.WireProperties(o => o.All());
             check = all_properties_were_injected;
@@ -15,10 +13,6 @@ namespace GIM.CastleContrib.Tests.PropertyInjectionFacility {
             create_facility = () => new OptionalPropertyInjectionFacility(true);
             additonal_registration = reg => reg.WireProperties(o => o.None());
             check = no_properties_were_injected;
-        }
-        [Fact] public void error_when_configuring_all_and_none_properties_to_inject() {
-            additonal_registration = reg => reg.WireProperties(o => o.All().None());
-            expect_error_while_registering<InvalidOperationException>();
         }
     }
 }
